08dd3347625c12532afb834b49a123d4f1a7479e
Yuhao-nyc <jimfengyh@gmail.com>
2016-11-10 17:41:27 -0500

    navbar added; js cleanup; inline styles

--- app/js/playlist.js
+++ app/js/playlist.js
@@ -1,8 +1,5 @@
 function YouTubePlayList(id, entries) {
 	this.id = id;
-	//this.entries = entries;
-	//this.currently_playing = 0;
-	//this.randomizer = false;
 }
 
 function linkify(text){
@@ -33,49 +30,35 @@ function addPlaylistToElement(playlist_id, element_id) {
 	request.execute(function(response) {
 
 		var output = '';
+		var output_img = '';
 		$.each( response.items, function( key, val ) {
 
 			var video_id = val.snippet.resourceId.videoId;
             var note = val.snippet.description;
             var linkifyNote = linkify(note);
+            var title = val.snippet.title;
+            var channelTitle = val.snippet.channelTitle;
+            var publishedAt = val.snippet.publishedAt;
 
             output += "<div class='playListEntry col-sm-4'>";
             output += "<iframe class='embed-responsive-item' width='100%' height='184px' src='https://www.youtube.com/embed/"+ video_id +"?list=;"+ player_id +"="+ video_id +"&amp;showinfo=0' frameborder='0' allowfullscreen></iframe>";
-            output += "<p class='playlistDesc'>"+ linkifyNote +"</p>";
+            output += "<h4 class='playlistTitle'>"+ title +"</h4>";
+            output += "<p class='playlistDesc'>by&nbsp;"+ channelTitle +"</p>";
             output += "</div>";
+
+            output_img += "<div class='playListEntry col-sm-1'>";
+            output_img += "<img onclick='switchIframe(this)' id='"+ video_id +"' class='img-fluid' width='100%' height='134px' src='https://img.youtube.com/vi/"+ video_id +"/maxresdefault.jpg'>";
+            output_img += "<h4 class='playlistTitle'>"+ title +"</h4>";
+            output_img += "<p class='playlistDesc'>"+ linkifyNote +"</p>";
+            output_img += "<p class='playlistDesc'>published at: "+ publishedAt +"</p>";
+            output_img += "</div>";
 
 		});
         $('#youtube-playList').append(output);
+        $('#youtube-playList-img').append(output_img);
         $('#youtube-playList > div').slice(-6).addClass("hidden-vid");
 
-	}, $('.spinner').fadeOut(400));
-
-    request.execute(function(response) {
-
-		var output = '';
-		$.each( response.items, function( key, val ) {
-
-			var video_id = val.snippet.resourceId.videoId;
-            var note = val.snippet.description;
-            var title = val.snippet.title;
-            var publishedAt = val.snippet.publishedAt;
-            //var formatDate = publishedAt.toString();
-            var channelTitle = val.snippet.channelTitle;
-            //var comment = val.snippet.comment;
-            var linkifyNote = linkify(note);
-
-            output += "<div class='playListEntry col-sm-1'>";
-            output += "<img onclick='switchIframe(this)' id='"+ video_id +"' class='img-fluid' width='100%' height='134px' src='https://img.youtube.com/vi/"+ video_id +"/maxresdefault.jpg'>";
-            output += "<h4 class='playlistTitle'>"+ title +"</h4>";
-            output += "<p class='playlistDesc'>"+ linkifyNote +"</p>";
-            output += "<p class='playlistDesc'>published at: "+ publishedAt +"</p>";
-            output += "</div>";
-
-		});
-        //console.log(month);
-        $('#youtube-playList-img').append(output);
-	}, $('.spinner-img').fadeOut(400));
-
+	}, $('.spinner, .spinner-img').fadeOut(360));
     $("#youtube-player").attr('src', 'https://www.youtube.com/embed/?list='+ player_id +'&amp;showinfo=0?enablejsapi=1');
 }
 
@@ -84,7 +67,7 @@ function switchIframe(clickedFrame) {
     $("#youtube-player").attr('src', 'https://www.youtube.com/embed/'+ videoframeId +'?list='+ playlistframeId +'&amp;showinfo=0?enablejsapi=1');
 };
 
-$("#load-more").click(function () {
+$("#player-load-more").click(function () {
     $(this).toggleClass('open');
     if ($(this).hasClass('open')) {
         $('.playListEntry.hidden-vid').fadeIn(230);
