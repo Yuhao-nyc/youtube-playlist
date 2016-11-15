eed262340c0c8f7b8b71a245f3aadea0a0a675c2
Yuhao-nyc <jimfengyh@gmail.com>
2016-11-15 12:48:40 -0500

    updates and fix

--- app/js/playlist.js
+++ app/js/playlist.js
@@ -1,7 +1,3 @@
-function YouTubePlayList(id, entries) {
-	this.id = id;
-}
-
 function linkify(text){
     if (text) {
         text = text.replace(
@@ -16,17 +12,17 @@ function linkify(text){
         );
     }
     return text;
-}
+};
 
 function addPlaylistToElement(playlist_id, element_id) {
 
 	var player_id = playlist_id;
 	var requestOptions = {
 		playlistId: playlist_id,
-		part: 'snippet',
+		part: "snippet,contentDetails",
         maxResults: 10
 	};
-	var request = gapi.client.youtube.playlistItems.list(requestOptions);
+	var request = gapi.client.youtube.playlistItems.list(requestOptions); //all kinds of data API here using only playlistItems (search, playlist etc etc)
 	request.execute(function(response) {
 
 		var output = '';
@@ -34,17 +30,12 @@ function addPlaylistToElement(playlist_id, element_id) {
 		$.each( response.items, function( key, val ) {
 
 			var video_id = val.snippet.resourceId.videoId;
-            var note = val.snippet.description;
-            var linkifyNote = linkify(note);
+            var desc = val.snippet.description;
+            var linkifyNote = linkify(desc);
             var title = val.snippet.title;
             var channelTitle = val.snippet.channelTitle;
             var publishedAt = val.snippet.publishedAt;
-
-            output += "<div class='playListEntry col-sm-4'>";
-            output += "<iframe class='embed-responsive-item' width='100%' height='184px' src='https://www.youtube.com/embed/"+ video_id +"?list=;"+ player_id +"="+ video_id +"&amp;showinfo=0' frameborder='0' allowfullscreen></iframe>";
-            output += "<h4 class='playlistTitle'>"+ title +"</h4>";
-            output += "<p class='playlistDesc'>by&nbsp;"+ channelTitle +"</p>";
-            output += "</div>";
+            var note = val.contentDetails.note;
 
             output_img += "<div class='playListEntry col-sm-1'>";
             output_img += "<img onclick='switchIframe(this)' id='"+ video_id +"' class='img-fluid img-playlist' width='100%' height='134px' src='https://img.youtube.com/vi/"+ video_id +"/maxresdefault.jpg'>";
@@ -53,14 +44,19 @@ function addPlaylistToElement(playlist_id, element_id) {
             output_img += "<p class='playlistDesc'>published at: "+ publishedAt +"</p>";
             output_img += "</div>";
 
+            output += "<div class='playListEntry col-sm-4'>";
+            output += "<iframe class='embed-responsive-item' width='100%' height='184px' src='https://www.youtube.com/embed/"+ video_id +"?list=;"+ player_id +"="+ video_id +"&amp;showinfo=0' frameborder='0' allowfullscreen></iframe>";
+            output += "<h4 class='playlistTitle'>"+ title +"</h4>";
+            output += "<p class='playlistDesc'>by&nbsp;"+ channelTitle +"</p>";
+            output += "</div>";
 		});
-        $('#youtube-playList').append(output);
         $('#youtube-playList-img').append(output_img);
+        $('#youtube-playList').append(output);
         $('#youtube-playList > div').slice(-6).addClass("hidden-vid");
 
 	}, $('.spinner, .spinner-img').fadeOut(360));
     $("#youtube-player").attr('src', 'https://www.youtube.com/embed/?list='+ player_id +'&amp;showinfo=0');
-}
+};
 
 function switchIframe(clickedFrame) {
     var videoframeId = clickedFrame.id;
